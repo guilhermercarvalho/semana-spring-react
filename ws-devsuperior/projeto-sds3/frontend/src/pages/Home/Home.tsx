286011920a31ad82e6a09e5e05c25123da536e0c
Guilherme Carvalho <guilhermercarvalho512@gmail.com>
2021-11-07 18:17:39 +0000

    Home, navigation

--- ws-devsuperior/projeto-sds3/frontend/src/pages/Home/Home.tsx
+++ ws-devsuperior/projeto-sds3/frontend/src/pages/Home/Home.tsx
@@ -1,5 +1,29 @@
+import { Footer, Navbar } from "components";
+import { Link } from "react-router-dom";
+
 function Home() {
-  return <div></div>;
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <div className="jumbotron">
+          <h1 className="display-4">DSVendas</h1>
+          <p className="lead">
+            Analise o desempenho das suas vendas por diferentes perspectivas
+          </p>
+          <hr />
+          <p>
+            Esta aplicação consiste em exibir um dashboard a partir de dados
+            fornecidos por um back end construído com Spring Boot.
+          </p>
+          <Link className="btn btn-primary btn-lg" to="/dashboard">
+            Acessar dashboard
+          </Link>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default Home;
