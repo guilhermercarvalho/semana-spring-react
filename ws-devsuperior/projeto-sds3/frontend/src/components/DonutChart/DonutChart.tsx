34a8019e8b5be861aecde1aa17e779f598ed32e9
Guilherme Carvalho <guilhermercarvalho512@gmail.com>
2021-11-07 19:05:48 +0000

    DonutChart integration

--- ws-devsuperior/projeto-sds3/frontend/src/components/DonutChart/DonutChart.tsx
+++ ws-devsuperior/projeto-sds3/frontend/src/components/DonutChart/DonutChart.tsx
@@ -2,6 +2,7 @@ import { BASE_URL } from "utils/requests";
 import { SaleSum } from "types/sale";
 import axios from "axios";
 import ReactApexChart from "react-apexcharts";
+import { useEffect, useState } from "react";
 
 type ChartData = {
   labels: string[];
@@ -9,16 +10,20 @@ type ChartData = {
 };
 
 function DonutChart() {
-  let chartData: ChartData = { labels: [], series: [] };
-
-  axios.get(`${BASE_URL}/sales/amount-by-seller`).then((res) => {
-    const data = res.data as SaleSum[];
-    const labels = data.map((el) => el.sellerName);
-    const series = data.map((el) => el.sum);
-
-    chartData = { labels, series };
-    console.log(chartData);
+  const [chartData, setChartData] = useState<ChartData>({
+    labels: [],
+    series: [],
   });
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales/amount-by-seller`).then((res) => {
+      const data = res.data as SaleSum[];
+      const labels = data.map((el) => el.sellerName);
+      const series = data.map((el) => el.sum);
+
+      setChartData({ labels, series });
+    });
+  }, []);
 
   const options = {
     legend: {
